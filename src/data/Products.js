e260e87ec3f1d5494cc0a0d8023bda83b7461760
lamminhhuy <lhuy00429@gmail.com>
2023-05-07 18:05:02 +0700

    update client

--- src/data/Products.js
+++ src/data/Products.js
@@ -1,5 +1,15 @@
 const products = [
-
+  {
+    _id: "1",
+    name: "Velcro Ballerinas For Girls  (Pink)",
+    image: "/images/6.png",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book",
+    price: 89,
+    countInStock: 3,
+    rating: 4,
+    numReviews: 4,
+  },
   {
     _id: "2",
     name: "Velcro Sneakers For Boys & Girls  (Blue)",
